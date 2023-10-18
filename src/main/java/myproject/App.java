5a04003954bd2fcf22ffa744e3703b2ac9808833
zqCui233 <cuizhiqing110@gmail.com>
2023-10-18 17:59:49 -0400

    assignment5

--- src/main/java/myproject/App.java
+++ src/main/java/myproject/App.java
@@ -2,9 +2,15 @@ package myproject;
 
 import com.pulumi.Context;
 import com.pulumi.Pulumi;
+import com.pulumi.aws.AwsFunctions;
 import com.pulumi.aws.ec2.*;
-import com.pulumi.aws.ec2.inputs.RouteTableRouteArgs;
+import com.pulumi.aws.ec2.inputs.*;
+import com.pulumi.aws.inputs.GetAvailabilityZonesArgs;
+import com.pulumi.core.Output;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class App {
@@ -13,13 +19,18 @@ public class App {
     }
 
     public static void stack(Context ctx) {
-        var config = ctx.config("aws");
-        String region = config.require("region");
-        config = ctx.config();
+        int[] index = {0};
+        int[] num = {3};
+        final var available = AwsFunctions.getAvailabilityZones(GetAvailabilityZonesArgs.builder()
+                .state("available")
+                .build());
+
+        Output<Integer> numOfAz = available.applyValue(getAvailabilityZonesResult -> getAvailabilityZonesResult.names().size());
+
+        var config = ctx.config();
         String cidr = config.require("vpcCidr");
-//        String cidr = "10.0.0.0/16";
-//        String[] cidrs = new String[] {"10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"};
-        String cidrs[] = CIDRSubnetCalculator.calculate(cidr, 6);
+        String cidrs[] = CIDRSubnetCalculator.calculate(cidr, num[0] * 2);
+
         var main = new Vpc("main", VpcArgs.builder()
                 .cidrBlock(cidr)
                 .tags(Map.of("Name", "myVpc"))
@@ -46,36 +57,114 @@ public class App {
                 .tags(Map.of("Name", "Private Route Table"))
                 .build());
 
-        for (int i = 1; i <= 3; i++) {
-            var publicSubnet = new Subnet("Public Subnet " + i, new SubnetArgs.Builder()
-                    .vpcId(main.id())
-//                    .cidrBlock("10.0." + (i - 1) + ".0/24")
-                    .cidrBlock(cidrs[i - 1])
-//                    .availabilityZone("us-east-1" + (char) ('a' + i - 1))
-                    .availabilityZone(region + (char) ('a' + i - 1))
-                    .tags(Map.of("Name", "Public Subnet " + i))
-                    .build());
+        var applicationSecurityGroup = new SecurityGroup("appSG", SecurityGroupArgs.builder()
+                .description("Allow inbound traffic")
+                .namePrefix("application-")
+                .vpcId(main.id())
+                .ingress(SecurityGroupIngressArgs.builder()
+                        .fromPort(8080)
+                        .toPort(8080)
+                        .cidrBlocks("0.0.0.0/0")
+                        .protocol("tcp")
+                        .build())
+                .egress(SecurityGroupEgressArgs.builder()
+                        .fromPort(0)
+                        .toPort(0)
+                        .protocol("-1")
+                        .cidrBlocks("0.0.0.0/0")
+                        .build())
+                .tags(Map.of("Name", "Application Security Group"))
+                .build());
 
-            var publicRouteTableAssociation = new RouteTableAssociation("PublicRouteTableAssoc" + i, RouteTableAssociationArgs.builder()
-                    .subnetId(publicSubnet.id())
-                    .routeTableId(publicRouteTable.id())
-                    .build());
+        var allowSSH = new SecurityGroupRule("allowSHH", SecurityGroupRuleArgs.builder()
+                .type("ingress")
+                .fromPort(22)
+                .toPort(22)
+                .protocol("tcp")
+                .cidrBlocks("0.0.0.0/0")
+                .securityGroupId(applicationSecurityGroup.id())
+                .build());
 
-            var privateSubnet = new Subnet("Private Subnet" + i, new SubnetArgs.Builder()
-                    .vpcId(main.id())
-//                    .cidrBlock("10.0." + (i + 2) + ".0/24")
-                    .cidrBlock(cidrs[i + 2])
-//                    .availabilityZone("us-east-1" + (char) ('a' + i + 2))
-                    .availabilityZone(region + (char) ('a' + i - 1))
-                    .tags(Map.of("Name", "Private Subnet " + i))
-                    .build());
+        var allowHttp = new SecurityGroupRule("allowHttp", SecurityGroupRuleArgs.builder()
+                .type("ingress")
+                .fromPort(80)
+                .toPort(80)
+                .protocol("tcp")
+                .cidrBlocks("0.0.0.0/0")
+                .securityGroupId(applicationSecurityGroup.id())
+                .build());
 
-            var privateRouteTableAssociation = new RouteTableAssociation("PrivateRouteTableAssoc" + i, RouteTableAssociationArgs.builder()
-                    .subnetId(privateSubnet.id())
-                    .routeTableId(privateRouteTable.id())
-                    .build());
-        }
+        var allowHttps = new SecurityGroupRule("allowHttps", SecurityGroupRuleArgs.builder()
+                .type("ingress")
+                .fromPort(443)
+                .toPort(443)
+                .protocol("tcp")
+                .cidrBlocks("0.0.0.0/0")
+                .securityGroupId(applicationSecurityGroup.id())
+                .build());
+
+        Output<List<String>> sgIds = applicationSecurityGroup.id().applyValue(id -> {
+            List<String> ids = new ArrayList<>();
+            ids.add(id);
+            return ids;
+        });
+
+        var ec2Key = new KeyPair("ec2Key", KeyPairArgs.builder()
+                .publicKey(config.require("pubKey"))
+                .build());
+
+        numOfAz.applyValue(n -> {
+            if (n >= 3) num[0] = 3;
+            else num[0] = 2;
+
+            for (int i = 0; i < num[0]; i++, index[0]++) {
+                int finalIndex = index[0];
+                var publicSubnet = new Subnet("Public Subnet " + (i + 1), new SubnetArgs.Builder()
+                        .vpcId(main.id())
+                        .cidrBlock(cidrs[i])
+                        .availabilityZone(available.applyValue(getAvailabilityZonesResult -> getAvailabilityZonesResult.names().get(finalIndex)))
+                        .tags(Map.of("Name", "Public Subnet " + (i + 1)))
+                        .build());
+
+                var publicRouteTableAssociation = new RouteTableAssociation("PublicRouteTableAssoc" + i, RouteTableAssociationArgs.builder()
+                        .subnetId(publicSubnet.id())
+                        .routeTableId(publicRouteTable.id())
+                        .build());
+
+                var privateSubnet = new Subnet("Private Subnet " + (i + 1), new SubnetArgs.Builder()
+                        .vpcId(main.id())
+                        .cidrBlock(cidrs[i + num[0]])
+                        .availabilityZone(available.applyValue(getAvailabilityZonesResult -> getAvailabilityZonesResult.names().get(finalIndex)))
+                        .tags(Map.of("Name", "Private Subnet " + (i + 1)))
+                        .build());
+
+                var privateRouteTableAssociation = new RouteTableAssociation("PrivateRouteTableAssoc" + (i + 1), RouteTableAssociationArgs.builder()
+                        .subnetId(privateSubnet.id())
+                        .routeTableId(privateRouteTable.id())
+                        .build());
+
+                if (i == num[0] - 1) {
+                    var webapp = new Instance("webapp", InstanceArgs.builder()
+                            .ami(config.require("amiId"))
+                            .associatePublicIpAddress(true)
+                            .subnetId(publicSubnet.id())
+                            .instanceType("t2.micro")
+                            .vpcSecurityGroupIds(sgIds)
+                            .rootBlockDevice(InstanceRootBlockDeviceArgs.builder()
+                                    .deleteOnTermination(true)
+                                    .volumeSize(25)
+                                    .volumeType("gp2")
+                                    .build())
+                            .keyName(ec2Key.keyName())
+                            .tags(Map.of("Name", "webapp"))
+                            .build());
+                }
+            }
+
+            return null;
+        });
 
     }
+
 
 }
